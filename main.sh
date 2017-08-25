e09df7ed5872ba4126a74b3374665a572b2b524a
OneTwoThree <paulpc.dev@gmail.com>
2017-08-25 10:18:47 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-url='./cmd1.txt'
+cmd="$@"
+config=$(cat ~/.remote/config)
+baseurl='https://raw.githubusercontent.com/'
+
+url="${baseurl}${config}/master/${cmd}"
 
 if curl --output /dev/null --silent --head --fail "$url"; then
   echo "URL exists: $url"
