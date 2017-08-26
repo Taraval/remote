a7f7d169d8693ea0e8aa20b8ee6b4bc1e5284a7b
taraval <taraval@usa-ca-sanleandro.org>
2017-08-26 11:31:45 -0700

    updated

--- list.sh
+++ list.sh
@@ -3,13 +3,13 @@
 cmd="$@"
 config=$(cat ~/.remote/config)
 baseurl='https://api.github.com/repos/'
-extralurl='/contents'
+extralurl='contents'
 
 url="${baseurl}${config}/${extralurl}"
 
 if curl --output /dev/null --silent --head --fail "$url"; then
   echo "URL exists: $url"
-  curl -s $url
+  curl -s $url | grep -e "name.*" -e "type.*file" | grep -A1 'type.*' | grep name | cut -d'"' -f 4- | cut -d'"' -f1
 else
   echo "URL does not exist: $url"
 fi
