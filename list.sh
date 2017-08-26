0ea3617c954890b1495873e3fa496317a5cd7f1a
taraval <taraval@usa-ca-sanleandro.org>
2017-08-26 11:33:36 -0700

    updated

--- list.sh
+++ list.sh
@@ -8,11 +8,8 @@ extralurl='contents'
 url="${baseurl}${config}/${extralurl}"
 
 if curl --output /dev/null --silent --head --fail "$url"; then
-  echo "URL exists: $url"
+  #echo "URL exists: $url"
   curl -s $url | grep -e "name.*" -e "type.*file" | grep -A1 'type.*' | grep name | cut -d'"' -f 4- | cut -d'"' -f1
 else
   echo "URL does not exist: $url"
 fi
-
-
-#curl -s https://api.github.com/repos/lassana/continuous-audiorecorder/contents | grep -e "name.*" -e "type.*file" | grep -A1 'type.*' | grep name | cut -d'"' -f 4- | cut -d'"' -f1
