b2c6d40163a513feea65deb9dc65dcce04ebd5a2
taraval <taraval@usa-ca-sanleandro.org>
2017-08-26 18:22:01 -0700

    added show

--- show.sh
+++ show.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+cmd="$@"
+config=$(cat ~/.remote/config)
+baseurl='https://api.github.com/repos/'
+extralurl='contents'
+
+url="${baseurl}${config}/${extralurl}/${cmd}"
+
+if curl --output /dev/null --silent --head --fail "$url"; then
+  #echo "URL exists: $url"
+  curl -s $url | sed 's/{//' | sed 's/}//' | sed 's/,//'
+else
+  echo "URL does not exist: $url"
+fi
